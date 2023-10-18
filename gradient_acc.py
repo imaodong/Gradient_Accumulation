2b7fb3ac1654310c45e295c54a45e3e8a71de975
Maodong Li <103551781+MaoDong9@users.noreply.github.com>
2023-10-18 19:10:59 +0800

    Update gradient_acc.py

--- gradient_acc.py
+++ gradient_acc.py
@@ -2,7 +2,7 @@ for i, (data, label) in enumerate(train_loader):
     pred = model(data)
     loss = criterion(pred, label)
 
-    loss = loss / accumulation_steps  
+    loss = loss / accumulation_steps  # 损失标准化，为了梯度稳定
     loss.backward()
 
     if (i + 1) % accumulation_steps == 0:
