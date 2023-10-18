7feb23e27b2c923c0efe2237509e1edd4735151a
Maodong Li <103551781+MaoDong9@users.noreply.github.com>
2023-10-18 19:05:25 +0800

    Update gradient_acc.py

--- gradient_acc.py
+++ gradient_acc.py
@@ -9,5 +9,3 @@ for i, (data, label) in enumerate(train_loader):
         optimizer.step()    
 
         optimizer.zero_grad()
-# 可能会有人对这里为什么需要除以 accumulation_steps 有疑问
-# 实际上是这样的，如果我们有一个大的batch的话，直接计算梯度进行更新，
